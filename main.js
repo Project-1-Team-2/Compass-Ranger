ea10929992f20f14743f7175b70e46a3c013784f
rkurbanov <ray.kurbanov@gmail.com>
2022-08-22 19:17:53 -0500

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+let map = L.map("myMap", {
+  center: [40, -100],
+  zoom: 6,
+});
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
